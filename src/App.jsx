a65bebdab5037b1dc5ba48e15d567c1e2b6cfc13
Sana <darkking7806@gmail.com>
2024-05-02 12:30:07 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Feature from "./components/Feature";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Preview from "./components/Preview";
@@ -13,6 +14,7 @@ export default function App() {
         <Feature />
         <Preview />
         <Testimonials />
+        <Footer />
       </div>
     </>
   );
